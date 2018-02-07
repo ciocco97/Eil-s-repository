040d05c23e2cff37e05508bbedcd4dd0825c9083
Alessandro Trainini <>
2018-02-07 22:30:21 +0100

    Aggiunta la classe personaggio

--- Models/src/models/ClasseCoordinalizzabile.java
+++ Models/src/models/ClasseCoordinalizzabile.java
@@ -10,7 +10,7 @@ public class ClasseCoordinalizzabile{
     private int x;
     private int y;
 /**
- * 
+ * Costruttore della classe che inizializza le coordinate passate come parametro
  * @param x Intero che indica la coordinata X
  * @param y Intero che indica la coordinata Y
  */
@@ -18,26 +18,43 @@ public class ClasseCoordinalizzabile{
         this.x = x;
         this.y = y;
     }
-
+/**
+ * Costruttore vuoto per inizializzare le coordinate nell'origine degli assi
+ */
     public ClasseCoordinalizzabile() {
+        this.x = 0;
+        this.y = 0;
     }
-
+/**
+ * 
+ * @return Intero che rappresenta la coordinata X
+ */
     public int getX() {
         return x;
     }
-
+/**
+ * @param x Intero che rappresenta la nuova coordinata X 
+ */
     public void setX(int x) {
         this.x = x;
     }
-
+/**
+ * 
+ * @return Intero che rappresenta la coordinata Y
+ */
     public int getY() {
         return y;
     }
-
+/**
+ * @param y Intero che rappresenta la nuova coordinata Y
+ */
     public void setY(int y) {
         this.y = y;
     }
-    
+/**
+ * Metodo che ritorna la distanza dell'entità dall'origine degli assi
+ * @return Double che rappresenta la distanza
+ */
     public double getDistanceFromOrigin()
     {
         return Math.sqrt(x*x + y*y);
