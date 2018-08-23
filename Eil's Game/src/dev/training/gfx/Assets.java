eecf81c4995f912469e190a9a09c1c22abf57cdb
ciocco97 <f.cremascoli@studenti.unibs.it>
2018-08-23 11:43:18 +0200

    Modifica classe Assets

--- Eil's Game/src/dev/training/gfx/Assets.java
+++ Eil's Game/src/dev/training/gfx/Assets.java
@@ -13,17 +13,21 @@ public class Assets {
     
     private static final int width = 150, height = 150;
     
-    public static BufferedImage player, dirt, grass, stone, tree, rock, selected, charapter;
+    public static BufferedImage player, dirt, grass, stone, tree, rock, selected, charapter, chapter_archer;
     
     public static void init() {
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/Sprite.png"));
         selected = sheet.crop(580, 0, 20, 20);
-        grass = sheet.crop(600, 0, 20, 20);
-        dirt = sheet.crop(620, 0, 20, 20);
-        stone = sheet.crop(640, 0, 20, 20);
+//        grass = sheet.crop(600, 0, 20, 20);
+        grass = sheet.crop(1047, 825, 348, 348);
+//        dirt = sheet.crop(620, 0, 20, 20);
+        dirt = sheet.crop(1395, 825, 348, 348);
+//        stone = sheet.crop(640, 0, 20, 20);
+        stone = sheet.crop(697, 825, 348, 348);
         rock = sheet.crop(width * 3, 0, width, height);
         
 //        player = sheet.crop(0, height + 10, 450, 700);
-        charapter = sheet.crop(680, 0, 20, 20);
+//        charapter = sheet.crop(680, 0, 20, 20);
+        charapter = sheet.crop(0, 825, 348, 348);
     }
 }
