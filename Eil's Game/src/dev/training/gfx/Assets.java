7db03aec9441a0cf36bd88e29fe9664ee2893d4e
ciocco97 <f.cremascoli@studenti.unibs.it>
2018-08-23 16:31:19 +0200

    Aggiunto omino che comunque non mi soddisfa

--- Eil's Game/src/dev/training/gfx/Assets.java
+++ Eil's Game/src/dev/training/gfx/Assets.java
@@ -17,17 +17,18 @@ public class Assets {
     
     public static void init() {
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/Sprite.png"));
-        selected = sheet.crop(580, 0, 20, 20);
-//        grass = sheet.crop(600, 0, 20, 20);
-        grass = sheet.crop(1047, 825, 348, 348);
-//        dirt = sheet.crop(620, 0, 20, 20);
-        dirt = sheet.crop(1395, 825, 348, 348);
-//        stone = sheet.crop(640, 0, 20, 20);
-        stone = sheet.crop(697, 825, 348, 348);
+//        selected = sheet.crop(580, 0, 20, 20);
+        selected = sheet.crop(697, 478, 348, 348);
+        grass = sheet.crop(600, 0, 20, 20);
+//        grass = sheet.crop(1047, 825, 348, 348);
+        dirt = sheet.crop(620, 0, 20, 20);
+//        dirt = sheet.crop(1395, 825, 348, 348);
+        stone = sheet.crop(640, 0, 20, 20);
+//        stone = sheet.crop(697, 825, 348, 348);
         rock = sheet.crop(width * 3, 0, width, height);
         
 //        player = sheet.crop(0, height + 10, 450, 700);
 //        charapter = sheet.crop(680, 0, 20, 20);
-        charapter = sheet.crop(0, 825, 348, 348);
+        charapter = sheet.crop(1491, 69, 348, 348);
     }
 }
