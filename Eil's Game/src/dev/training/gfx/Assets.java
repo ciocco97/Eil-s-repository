9009bbbc8d45da466f9de05a10836166aecebad1
ciocco97 <f.cremascoli@studenti.unibs.it>
2018-08-29 11:56:46 +0200

    Modificato assets per selezione sprite

--- Eil's Game/src/dev/training/gfx/Assets.java
+++ Eil's Game/src/dev/training/gfx/Assets.java
@@ -18,18 +18,18 @@ public class Assets {
     public static void init() {
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/Sprite.png"));
 //        selected = sheet.crop(580, 0, 20, 20);
-        selected = sheet.crop(697, 478, 348, 348);
-        grass = sheet.crop(600, 0, 20, 20);
+        selected = sheet.crop(160, 0, 20, 20);
+        grass = sheet.crop(40, 0, 20, 20);
 //        grass = sheet.crop(1047, 825, 348, 348);
-        dirt = sheet.crop(620, 0, 20, 20);
+        dirt = sheet.crop(60, 0, 20, 20);
 //        dirt = sheet.crop(1395, 825, 348, 348);
-        stone = sheet.crop(640, 0, 20, 20);
+        stone = sheet.crop(80, 0, 20, 20);
 //        stone = sheet.crop(697, 825, 348, 348);
 //        rock = sheet.crop(width * 3, 0, width, height);
         
 //        player = sheet.crop(0, height + 10, 450, 700);
 //        charapter = sheet.crop(680, 0, 20, 20);
-        chapter_soldier100 = chapter_soldier50 = chapter_soldier10 = sheet.crop(1491, 69, 348, 348);
-        chapter_archer100 = chapter_archer50 = chapter_archer10 = sheet.crop(351, 825, 348, 348);
+        chapter_soldier100 = chapter_soldier50 = chapter_soldier10 = sheet.crop(0, 29, 350, 350);
+        chapter_archer100 = chapter_archer50 = chapter_archer10 = sheet.crop(0, 380, 350, 350);
     }
 }
