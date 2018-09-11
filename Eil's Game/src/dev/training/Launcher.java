7a4975f12b5431f1d9e57972a8c59b8308255e36
ciocco97 <f.cremascoli@studenti.unibs.it>
2018-09-11 17:44:43 +0200

    Fine attacco arceri con send

--- Eil's Game/src/dev/training/Launcher.java
+++ Eil's Game/src/dev/training/Launcher.java
@@ -1,13 +1,15 @@
 package dev.training;
 
+import java.awt.Toolkit;
+
 public class Launcher {
 
     @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public static void main(String[] args) {
         String title = "Training";
-        int width = 1400;//Toolkit.getDefaultToolkit().getScreenSize().width;
-        int height = 700;//Toolkit.getDefaultToolkit().getScreenSize().height;
-        String serverAddress = "localhost";
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+        String serverAddress = "25.82.144.168";
         Game game = new Game(title, serverAddress, width, height);
         game.start();
     }
