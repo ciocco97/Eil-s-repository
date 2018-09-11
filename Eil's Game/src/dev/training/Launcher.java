66b5bb12b7376875723138b2d145c097c882850b
ciocco97 <f.cremascoli@studenti.unibs.it>
2018-09-11 22:35:55 +0200

    Attacco completo client

--- Eil's Game/src/dev/training/Launcher.java
+++ Eil's Game/src/dev/training/Launcher.java
@@ -9,7 +9,7 @@ public class Launcher {
         String title = "Training";
         int width = Toolkit.getDefaultToolkit().getScreenSize().width;
         int height = Toolkit.getDefaultToolkit().getScreenSize().height;
-        String serverAddress = "25.82.144.168";
+        String serverAddress = "localhost";
         Game game = new Game(title, serverAddress, width, height);
         game.start();
     }
